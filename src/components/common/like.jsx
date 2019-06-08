66806900cf508c64988a0f9910634ddb88f1af29
benjamin-karami <51356198+benjamin-karami@users.noreply.github.com>
2019-06-08 08:40:45 +0430

    Add files via upload

---
+++ src/components/common/like.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+const Like = props => {
+  let classes = !props.liked ? "fa fa-heart-o" : "fa fa-heart";
+  return (
+    <i
+      style={{ cursor: "pointer" }}
+      onClick={props.onClick}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
+
+export default Like;
