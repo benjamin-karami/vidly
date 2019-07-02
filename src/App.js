a63490f853264b6747f15dcd8b392051b618e79e
Benyamin Karami <b.karami@parham-co.ir>
2019-07-02 14:11:13 +0430

    Route Complete 110

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
-import "./App.css";
+import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <main className="container">
-        <Route path="/movies" component={Movies} />
-        <Route path="/customers" component={Customers} />
-        <Route path="/rental" component={Rentals} />
-        <Route path="/notFound" component={NotFound} />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rental" component={Rentals} />
+            <Route path="/not-Found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
