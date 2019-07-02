4b6f16d29780949f725a0e6b3402c9f1afcaac29
Benyamin Karami <b.karami@parham-co.ir>
2019-07-02 12:26:32 +0430

    adding Routing 107

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import Movies from "./components/movies";
 import "./App.css";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   state = {};
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rental" component={Rentals} />
+        <Route path="/notFound" component={NotFound} />
       </main>
     );
   }
